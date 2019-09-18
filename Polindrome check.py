096fdce3bdcce7ab7491834d565ea8a159bbb359
Viktor <45604832+KvaziDeveloper1703@users.noreply.github.com>
2019-09-18 13:24:52 +0300

    Add files via upload

---
+++ Polindrome check.py
@@ -0,0 +1,11 @@
+choice = 1;
+while choice == 1:
+    textLine = input("Write a string here, to check if it is a polindrome:") #We enter the line
+    reversedTextLine = textLine[::-1] #We reverse the line entered
+    casefoldTextLine = textLine.casefold() #We get rid of capital letters, so program can also work with capitalized sentences 
+    casefoldReversedTextLine = reversedTextLine.casefold()
+    if casefoldTextLine == casefoldReversedTextLine:
+        print("It is a polindrome")
+    else:
+        print("It is not a polindrome")
+    choice = int(input("Do you want to check other string?[Yes-1/No-0]")) #Here user can choose to continue or stop using this program
